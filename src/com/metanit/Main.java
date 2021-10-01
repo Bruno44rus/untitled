c3b9bb393780347bc1535eb15817c8f32c3ba7e4
bruno44rus <kirrrill007@mail.ru>
2021-10-01 09:57:56 +0300

    Initial commit

---
+++ src/com/metanit/Main.java
@@ -0,0 +1,39 @@
+package com.metanit;
+
+public class userRepository
+{
+    user[] users = new user[10];
+
+    public void user()
+    {
+        int id;
+        string name;
+        string surname;
+
+    }
+
+    public void addUser()
+    {
+
+    }
+
+    public void getUser(int id)
+    {
+
+    }
+
+    public void getOrderedUsers()
+    {
+
+    }
+
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Java!");
+    }
+}
